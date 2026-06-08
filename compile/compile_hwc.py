88125f70937571e16c815a8c37b382cdb38af709
JunsuHA <junsu000306@gmail.com>
2026-06-08 16:04:05 +0900

    .

--- compile/compile_hwc.py
+++ compile/compile_hwc.py
@@ -27,7 +27,7 @@ Then inside container:
 
 import glob
 import os
-from qubee import mxq_compile
+from qbcompiler import mxq_compile
 
 BASE_DIR   = os.path.dirname(os.path.abspath(__file__))          # .../npu/compile
 ASSETS_DIR = os.path.normpath(os.path.join(BASE_DIR, "..", "assets"))
